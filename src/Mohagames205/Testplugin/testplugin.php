9fbab9d28b041a75a6867f7d0084c163d7c5b050
Mohamed El Yousfi <mohamedelyousfi2003@gmail.com>
2019-02-17 22:24:59 +0100

    Update testplugin.php

--- src/Mohagames205/Testplugin/testplugin.php
+++ src/Mohagames205/Testplugin/testplugin.php
@@ -4,12 +4,10 @@ namespace Testplugin;
 
 use pocketmine\utils\TextFormat as C;
 use pocketmin\plugin\PluginBase;
-use Testplugin\main;
+use Testplugin\Main;
 
-class Main extends PluginBase{
+class Main extends PluginBase {
     public function onEnable(){
         $this->getLogger()->info(C::GREEN . "Plugin is geactiveerd!");
-
     }
-
 }
