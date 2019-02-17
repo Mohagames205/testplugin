ed792ee1a7b39e075689ab17a0c1b5bf5d3ab2ea
Mohamed El Yousfi <mohamedelyousfi2003@gmail.com>
2019-02-17 22:44:10 +0100

    e

--- src/Mohagames205/Testplugin/Main.php
+++ src/Mohagames205/Testplugin/Main.php
@@ -5,7 +5,7 @@ namespace Testplugin;
 use pocketmine\utils\TextFormat as C;
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase {
+class test extends PluginBase {
     public function onEnable(){
         $this->getLogger()->info(C::GREEN . "Plugin is geactiveerd!");
     }
