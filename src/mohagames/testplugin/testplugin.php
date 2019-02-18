9a1cede08bf9169ca856ed16c427c8b8d7dabc67
Mohamed El Yousfi <mohamedelyousfi2003@gmail.com>
2019-02-18 21:40:11 +0100

    Update testplugin.php

--- src/mohagames/testplugin/testplugin.php
+++ src/mohagames/testplugin/testplugin.php
@@ -4,9 +4,24 @@ namespace mohagames\testplugin;
 
 use pocketmine\utils\TextFormat as C;
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class testplugin extends PluginBase {
     public function onEnable(){
         $this->getLogger()->info(C::GREEN . "JAAAAAAAAA MIJN PLUGIN WERKT");
+
+    }
+
+    public function on_Command(CommandSender $sender, Command $command, string $label, array $args) : bool{
+        switch($command->getName()){
+            case "test":
+                $sender->sendMessage("Hey hoi" . $sender->getName());
+
+                return true;
+            default:
+                return false;
+                
+        }
     }
 }
